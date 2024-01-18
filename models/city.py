696df024def92a16a778b02670aa136fa64e892c
dadovenonye <chinonyenmecha@yahoo.com>
2024-01-17 19:27:37 -0800

    updated the city class

--- models/city.py
+++ models/city.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
+from sqlalchemy.ext.declarative import declarative_base
 
-class City(BaseModel):
+Base = declarative_base()
+
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
 
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
     places = relationship('Place', backref='cities',
-                cascade='all, delete-orphan')
+                          cascade='all, delete-orphan')
+
