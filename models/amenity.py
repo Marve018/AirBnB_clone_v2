eaa7192ae39cbec86e69d04937122ca6e9ccb6b8
dadovenonye <chinonyenmecha@yahoo.com>
2024-01-18 19:35:09 -0800

    made corrections

--- models/amenity.py
+++ models/amenity.py
@@ -2,7 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from place import place_amenity
+from .place import place_amenity
 from sqlalchemy.orm import relationship
 
 
