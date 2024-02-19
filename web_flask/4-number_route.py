873ed34e45f77dd1052f246fe17c56f72dadaf90
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-02-19 07:57:53 +0100

    {fix}: PEP8 style

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -6,21 +6,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """ Routing to root """
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ Routing to /hbnb page"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """Routing to C using Variables"""
     text = text.replace('_', ' ')
     return "C {}".format(text)
+
 
 @app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,10 +33,12 @@ def python_is_cool(text):
     text = text.replace('_', ' ')
     return "Python {}".format(text)
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def is_a_numbet(n):
     """Routing to n for intergers"""
     return "{} is a number".format(n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
