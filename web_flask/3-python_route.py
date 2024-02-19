40a9bc1260c7b1a0e07bf1e67e4b2eb6977dfa8b
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-02-19 07:57:47 +0100

    {fix}: PEP8 style

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -6,15 +6,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """ Routing to root """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """ Routing to /hbnb page"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
@@ -22,12 +25,14 @@ def c_is_fun(text):
     text = text.replace('_', ' ')
     return "C {}".format(text)
 
+
 @app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is_cool(text):
     """Routing to python with defult option using Variables"""
     text = text.replace('_', ' ')
     return "Python {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
